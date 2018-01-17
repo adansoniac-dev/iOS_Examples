924715b071c9e5c35440d7e3003eb1c5f62968b6
Obi <adansoniactestdev@gmail.com>
2018-01-17 07:14:50 -0600

    Swift manual understanding

---
+++ Master_Swift4.0/Basic_Operators.playground/Contents.swift
@@ -0,0 +1,44 @@
+//      BASIC OPERATORS
+
+
+// Assignment operator
+let b = 10
+var a = 5
+a = b
+print(b)
+
+let (x, y) = (1, 2)
+print(x, y)
+
+if x == y {
+    print("x matches y")
+} else {
+    print("A common mistake made is to use = instead of == ")
+}
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+
+"hello, " + "world"
+
+9 % 4
+
+
+// Comparisons
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+
+let name = "world"
+if name == "world" {
+    print("hello, \(name)")
+} else {
+    print("I'm sorry \(name), but I don't recognize you")
+}
+
