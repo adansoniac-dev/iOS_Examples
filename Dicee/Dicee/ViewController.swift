7bafe4d42ac770c02e074f85f2ece53d2c67bd9f
Obi <adansoniactestdev@gmail.com>
2018-01-18 07:46:10 -0600

    updates to Dicee

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var randomDiceIndex1 : Int = 0
-    var randomDiceIndex2 : Int = 0
+    var randomDiceIndex1 : Int = 0              // to contain random number generated between 1 & 6 for dice 1
+    var randomDiceIndex2 : Int = 0              // to contain random number generated between 1 & 6 for dice 2
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    // array contains the name of dice images simulating sides of a die
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        randomDiceFaces()       // calls the function that sets images
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +33,26 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceFaces()      // calls the function that sets images
+        
+    }
+    
+    func randomDiceFaces() {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))          // creates random number between 1 & 6
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
-        print(randomDiceIndex2)
+        // set image views randomly ...
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
+    }
+    
+    // created this to work by shaking the phone
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         
+        randomDiceFaces()
+    
     }
     
 }
